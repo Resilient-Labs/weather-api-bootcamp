4644135e04c37a8c5184574d571f84298d66880f
Anthony Salmeron <anthonysalmerondev@gmail.com>
2019-10-29 19:22:51 -0400

    weather api

---
+++ main.js
@@ -0,0 +1,16 @@
+// let key = "8adfbe69f67a9591d1b17f85c531907b";
+let city,countryCode;
+document.querySelector("button").onclick= () => {
+  city = document.getElementById("city").value;
+  countryCode = document.getElementById("country").value.toLowerCase();
+  if(city!=""&&countryCode!=""){
+    var p;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=imperial&APPID=8adfbe69f67a9591d1b17f85c531907b`)
+      .then(response => response.json())
+      .then(response => {
+        p = String(response.main.temp);
+        console.log(typeof p);
+        document.querySelector("span").textContent = p;
+      });
+  }
+};
