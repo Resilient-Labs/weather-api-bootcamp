d22c56637b3a363e52cb3c725cbc9554e9f35a73
Ahmed Dev <a.ahmed9316@gmail.com>
2019-11-10 17:52:29 -0500

    secondcommit

--- weather api/main.js
+++ weather api/main.js
@@ -1,22 +1,27 @@
 
 
 const button=document.querySelector("#button")
-const result=document.querySelector('#result')
+let result=document.querySelector('#result')
 
 let key='e7c585b428ccbab2374f86e9e8b04898'
 
+
 button.addEventListener('click',()=>{
-  const countrycode=document.querySelector('#countrycode').value
-  const cityname=document.querySelector('#cityname').value
+
+  let countrycode=document.querySelector('#countrycode').value
+  let cityname=document.querySelector('#cityname').value
+
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname},${countrycode}&APPID=${key}`)
   .then(res=>res.json())
   .then(response=>{
     let far=Math.floor(response.main.temp * 9/5-459.67)
-    let v=document.createTextNode(far)
-    result.appendChild(v)
-    
+      let v=document.createTextNode(`${far} degrees F `)
+      result.appendChild(v)
+      result=""
+
+
 
 }).catch(err=>{
-  alert('Sorry')
+  alert('Enter New Address')
 })
 })
