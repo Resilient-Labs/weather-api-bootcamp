64628327ea242423ae9e430658f72a04c2da988d
Johnbel Mahautiere <47404011+JohnbelMDev@users.noreply.github.com>
2020-09-06 22:54:29 -0400

    Update ApiObject.js

--- Weather API /JS/ApiObject.js
+++ Weather API /JS/ApiObject.js
@@ -5,7 +5,7 @@ let objectApi ={
   h2:document.querySelector('h2'),
   button:document.querySelector('button'),
   apiProperties:function fetchingApi(){
-          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${objectApi.city.value},${objectApi.country.value}&appid=36f1baad2895dd4173702c1fac524cd9&units=imperial`)
+          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${objectApi.city.value},${objectApi.country.value}&appid=8901884e0c76eb846f6f649e7c89e3fa&units=imperial`)
           .then(response => response.json())
           .then(data => {
             document.querySelector('h2').innerHTML=data.main.temp;
