32dc28f7ae1051240cbee93647d9d01d0ce9e48c
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-03-05 04:53:46 -0500

    WeatherAPIAnswer
    I completed the challenge:5
    I feel good about my code:5

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//what I have to do 
+//User will set the city and country. Use that location to get the weather of the day from that location 
+//get api fro weather satellite
+
+//add event listener for the button 
+document.querySelector('button').addEventListener('click', getWeather)
+
+//create a function 
+function getWeather(){
+    let selectCity=document.querySelector('#inputCity').value
+    let selectCountry=document.querySelector('#inputCountry').value
+    console.log(selectCity)
+    console.log(selectCountry)
+
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${selectCity},${selectCountry}&APPID=408ed3076a7c1b01ae64d051a21df0a0`
+
+    //'fetch' is connected to the browser
+    //console--object
+    //log--function
+    fetch (url)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        //temp has to be converted from K to F
+        document.querySelector('h3').innerHTML=(data.main.temp*(9 / 5) - 459.67)
+    })
+    .catch(err=>{
+        console.log(`error${err}`)
+    })
+}
