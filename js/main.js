7d9d535f7753fa5e30ae4880f67ae28ed1fd86fd
duane <duaneenrique26@gmail.com>
2021-10-02 19:05:37 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-
 // CASS group help work together with the last .thendata to get the temp to dispaly. 
 
 document.querySelector('button').addEventListener('click', weather)
@@ -7,13 +5,13 @@ document.querySelector('button').addEventListener('click', weather)
 function weather(){
     const city = document.querySelector('.city').value
     const country = document.querySelector('.country').value
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${country}&units=imperial&appid=114091f0852c3d6b1adbfa85bf4106b7`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${country}&units=imperial&appid=7aa8a92b00737d0587f7ef9f7f7e33c7`
     fetch(url)
     .then(response => {
         console.log(response)
         return response.json()
         })
-        .then(data =>{document.querySelector('h3').innerText=data.main.temp
+        .then(data =>{document.querySelector('h3').innerText= data.main.temp
     console.log(data)
     })
 }
