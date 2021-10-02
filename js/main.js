b45d047752cc9645f6f78c5d96843889aff7acff
Natasha Torres <natashaxtorres@gmail.com>
2021-10-01 22:02:25 -0400

    key issues

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.querySelector('button').addEventListener('click', todaysWeather)
 function todaysWeather() {
 
     let val = document.querySelector('input').value
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${val}&units=imperial&appid=15959b4b3e8c6f98e779c9767d5a02c0`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${val}&units=imperial&appid=KEY`
 
     fetch(url)
         .then(res => res.json())
