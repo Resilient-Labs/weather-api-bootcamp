f4f6e9278fc5913711810175d5edc9d0b6787bdb
Enaisha <Enaisha15@gmail.com>
2021-10-01 20:53:52 -0400

    EnaishaWeatherApi

---
+++ WeatherAPI copy/js/main.js
@@ -0,0 +1,22 @@
+document.querySelector("#submit").addEventListener("click", weather)
+
+function weather(){
+    let city = document.querySelector('#city').value
+    let state = document.querySelector('#state').value
+    let country = document.querySelector('#country').value
+    let location = city + "," + state + "," + country
+    const url = ("https://api.openweathermap.org/data/2.5/weather?q=" + `${location}` + "&appid=822b8faf46b7ece382f979b9814116ba")
+    
+
+fetch(url)
+.then(res => res.json()) 
+.then(data => {
+kelvinToFarenheit(data.main.temp)
+})
+}
+
+function kelvinToFarenheit(k){
+    let city = document.querySelector('#city').value
+    kToF = Math.floor((k - 273.15) * 9/5 + 32)
+    document.querySelector("#returnedTemp").innerText = 'The temperature in ' + city.toString() + ' is ' + kToF
+}
