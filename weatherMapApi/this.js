5765a72b7a0b2cf9086e8cbf2bc9ae7df5f6a8cd
samekh resh <samekh.resh@gmail.com>
2021-10-02 21:11:59 -0400

    made minor css changes and in order to make this work, currently, you will need to use your own API key, why? safety reasons.

--- weatherMapApi/this.js
+++ weatherMapApi/this.js
@@ -1,8 +1,5 @@
-
 let button = document.querySelector('button')
 button.addEventListener('click', findWeather)
-
-
 
 function findWeather(){
   let cityname = document.querySelector('#city').value
@@ -10,15 +7,12 @@ function findWeather(){
   let statename = ''
   let findLocation = cityname + ',' + statename + ',' + countrycode
   
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${findLocation}&appid=bcd38d1b6d16b6ef3a872197b4588665`
-  
-  
-  
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${findLocation}&appid=ur_not_getting_my_Key`
   fetch(url)
     .then(res => res.json())
     .then(data => {
       console.log(data.main)
       let weatherTemp = (data.main.temp - 273.15)*9/5 +32
-      document.querySelector('#weatherResults').innerText = `the current temperature is: ${weatherTemp} degree Fahrenheit`
+      document.querySelector('#weatherResults').innerText = `the current temperature is: ${weatherTemp} degrees Fahrenheit`
     })
 }
