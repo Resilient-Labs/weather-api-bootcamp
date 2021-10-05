4445d90c53114fb0e01ef86dcbb893b102fdc81d
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-10-04 20:04:01 -0400

    Improved styling, added forecast feature, updated README and upload of project demo

---
+++ README.md
@@ -0,0 +1,19 @@
+# 🌤 ☔️ Weather App ☔️ 🌤
+
+This weather app uses a weather API to display the temperature and a brief forecast for a provided, city, state and country
+
+## Link to Project: 
+
+![Weather App Demo](url)
+
+
+## How It's Made:
+Tech used: HTML, CSS, JavaScript, OpenWeather API
+
+
+## Optimizations:
+I enhanced this simple app by providing forecast details in addition to the temperature
+
+
+## Lessons Learned:
+I used the OpenWeather API to create this project. It was a great opportunity to continue the practice of using APIs and to query specific units of temperature.
--- js/main.js
+++ js/main.js
@@ -8,16 +8,18 @@ function getTemperature(){
   let state = document.querySelector('#state').value
   let country = document.querySelector('#country').value
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&units=imperial&appid=__________________________`
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&units=imperial&appid=efe853eab315f356609db78afe217b4a` //line 11
+
+  document.querySelector('p').hidden = true
 
   fetch(url)
       .then(res => res.json())
       .then(data => {
-        tempDisplay.innerText = data.main.temp
-        console.log(data)
+        tempDisplay.hidden = false
+        tempDisplay.innerText = "It's " + data.main.temp + ' degres F with ' + data.weather[0].description
       })
       .catch(err => {
-        document.querySelector('h2').innerText = 'Oh, no! There seems to be an error'
+        document.querySelector('h2').innerText = "Can't find that location. Try again"
           console.log(`error ${err}`)
   });
 }
