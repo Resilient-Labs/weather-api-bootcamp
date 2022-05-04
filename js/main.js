0b430b9956b967361da20c1fb79bfb086b336114
highfivenick <nickhawkins.dev@gmail.com>
2022-05-04 13:36:55 -0400

    cleaned up js

--- js/main.js
+++ js/main.js
@@ -9,18 +9,6 @@ function getWeather(){
     .then (res => res.json())
     .then (data => {
         console.log(data)
-        // console.log(data.current.condition.text)
-        // console.log(data.current.condition.icon)
-        // console.log(data.current.temp_f)
-        // console.log(data.current.feelslike_f)
-        // console.log(data.current.wind_dir)
-        // console.log(data.current.wind_mph)
-        // console.log(data.current.humidity)
-        // console.log(data.current.last_updated)
-        // console.log(data.current.precip_in)
-        // console.log(data.location.country)
-        // console.log(data.location.name)
-        // console.log(data.location.region)
 
         document.querySelector('h2').innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`
 
@@ -42,22 +30,3 @@ function getWeather(){
     })
 }
 
-// fcaf8e958e904ef1bf1191928220305
-
-// function getPhoto(){
-//     let date = document.querySelector('input').value
-//     console.log(date)
-//     let url = `https://api.nasa.gov/planetary/apod?api_key=JKqBx7bTAjQqQZAZM15dUVOoqqfJXFRHVckqRlfL&date=${date}`
-//     console.log(url)
-//     fetch (url)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         document.querySelector('img').src = data.hdurl
-//         document.querySelector('h3').innerText = data.explanation
-//         document.querySelector('h2').innerText = data.title
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     })
-// }
