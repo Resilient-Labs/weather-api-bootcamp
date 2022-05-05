60c6219c47007f189521b897d6567cc7814d431d
Cyd Villavicencio <villacv@gmail.com>
2022-05-05 19:27:21 -0400

    added open weather api fetch call and added inputs to allow user to enter a city, state, and a country to return the weather in fahrenheit

---
+++ script.js
@@ -0,0 +1,23 @@
+const api_key = config.MY_KEY
+
+const getInfo = (e) => {
+const city = document.getElementById('city').value
+const state = document.getElementById('state').value
+const country = document.getElementById('country').value
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${api_key}&units=imperial`
+    console.log(api)
+    e.preventDefault()
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('displayCity').innerHTML = data.name 
+        document.getElementById('displayWeather').innerHTML = data.main.temp 
+        // document.getElementById('display').innerHTML = data.wind.speed
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
+
+document.querySelector('#button').addEventListener('click', getInfo)
